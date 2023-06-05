a9ade7a0ffe239ecc43fad4ccdd6f6226d64fed0
AregAvetisyan82 <areg.avetisyan+1@gmail.com>
2023-06-05 13:46:19 +0400

    New change3

---
+++ run-tests1.sh
@@ -0,0 +1,3 @@
+pytest  --browser chrome
+#pytest --browser chrome --alluredir=/Users/areg.avetisyan/PycharmProjects/BDG/pythonProject1/reports
+#allure serve /Users/areg.avetisyan/PycharmProjects/BDG/pythonProject1/reports
